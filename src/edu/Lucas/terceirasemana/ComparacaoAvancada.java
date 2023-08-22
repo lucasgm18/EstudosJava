9faf1e0a767bd238c4d816e28d7bf435d5321a3e
Lukita18 <lucasgomesm1808@gmail.com>
2023-08-21 22:00:38 -0300

    Adicionando Comparação avançada 1

---
+++ src/edu/Lucas/terceirasemana/ComparacaoAvancada.java
@@ -0,0 +1,24 @@
+package edu.Lucas.terceirasemana;
+
+public class ComparacaoAvancada {
+    public static void main(String[] args) {
+
+        String nome1 = "JAVA";
+        String nome2 = "JAVA";
+
+        System.out.println(nome1 == nome2); //true
+
+        String nome3 = new String("JAVA");
+
+        System.out.println(nome1 == nome3); //false
+
+        String nome4 = nome3;
+
+        System.out.println(nome3 == nome4); //true
+
+        //equals na parada
+        System.out.println(nome1.equals(nome2)); //TRUE
+        System.out.println(nome2.equals(nome3)); //TRUE
+        System.out.println(nome3.equals(nome4)); //TRUE
+    }
+}
